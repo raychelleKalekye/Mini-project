880828d1f51f53528fdc4383313b813cb97f4535
LeonMunene <leon.bundi@strathmore.edu>
2024-10-31 19:57:10 +0300

    8-ball lighting

--- ball.py
+++ ball.py
@@ -1,11 +1,14 @@
 import math
 import cairo
+import random
 
 #  canvas dimensions
 WIDTH, HEIGHT = 600, 600
 BALL_RADIUS = 200
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 context = cairo.Context(surface)
+context.set_source_rgb(2/255,85/255,38/255)
+context.paint()
 
 
 def draw_sphere(context, center_x, center_y, radius):
@@ -17,6 +20,10 @@ def draw_sphere(context, center_x, center_y, radius):
 
 # main circle for the ball
     context.arc(center_x, center_y, radius, 0, 2 * math.pi)
+    gradient=cairo.RadialGradient(center_x - radius * 0.5, center_y - radius * 0.5, radius * 0.1, center_x, center_y, radius)
+    gradient.add_color_stop_rgb(0, 0.3, 0.3, 0.3) 
+    gradient.add_color_stop_rgb(0.7, 0.09, 0.09, 0.09)       
+    context.set_source(gradient)
     context.fill()
 #white circle
     shading = cairo.RadialGradient(center_x + radius * 0.2, center_y - radius * 0.2, radius * 0.05,
@@ -26,8 +33,8 @@ def draw_sphere(context, center_x, center_y, radius):
     shading.add_color_stop_rgb(1, 0.9, 0.9, 0.9)  # Slightly grey edge
     context.set_source(shading)  # Set color to white
     
-    white_circle_x = center_x + radius * 0.2  
-    white_circle_y = center_y - radius * 0.2
+    white_circle_x = center_x 
+    white_circle_y = center_y 
     context.arc(white_circle_x, white_circle_y, radius * 0.4, 0, 2 * math.pi)
     context.fill()
 
@@ -43,9 +50,10 @@ def draw_sphere(context, center_x, center_y, radius):
  #  "8" number 
     context.set_source_rgb(0, 0, 0)  # Set color to black for the number
     context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-    context.set_font_size(radius * 0.4)
-    context.move_to(white_circle_x - 25, white_circle_y + 25)  # Adjust position of the "8"
+    context.set_font_size(radius * 0.5)
+    context.move_to(white_circle_x - 29, white_circle_y + 34)  # Adjust position of the "8"
     context.show_text("8")
+
 
 # the 3D pool ball on the canvas
 draw_sphere(context, WIDTH // 2, HEIGHT // 2, 200)
